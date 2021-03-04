36244f4d858efc50f1ab0a01933cb43ed8c0ae73
Jeff Wooden <jeffwooden.courriel@gmail.com>
2021-03-04 21:46:02 +0100

    Added dotenv module to index file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
 const config = require("./config.json")
+require("dotenv").config();
 
 client.once("ready", () => {
     console.log("Ready !")
