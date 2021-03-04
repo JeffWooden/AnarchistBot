db48b7610c55d8623c43f38007bf3de1e00e982e
Jeff Wooden <jeffwooden.courriel@gmail.com>
2021-03-04 21:58:51 +0100

    Added start message

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const config = require("./config.json")
 require("dotenv").config();
 
 bot.once("ready", () => {
-    console.log("Ready !")
+    console.log(`${bot.user.tag} connecté.`)
+    if(config.startMessage == true) bot.channels.cache.find(ch => ch.name === "général").send("Hello World ! I'm UP !")
 })
 
 bot.login(process.env.TOKEN)
