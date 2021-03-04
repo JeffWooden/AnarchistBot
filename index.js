0f96ffd87c0c5dc49ba6ee644baf4e2ada603084
Jeff Wooden <jeffwooden.courriel@gmail.com>
2021-03-04 22:12:31 +0100

    added command handler

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
+const fs = require("fs")
 const Discord = require("discord.js")
 const bot = new Discord.Client()
-const config = require("./config.json")
+const commands = new Discord.Collection()
+const {prefix, startMessage, handlerPath} = require("./config.json")
 require("dotenv").config();
 
 bot.once("ready", () => {
     console.log(`${bot.user.tag} connecté.`)
-    if(config.startMessage == true) bot.channels.cache.find(ch => ch.name === "général").send("Hello World ! I'm UP !")
+    if(startMessage == true) bot.channels.cache.find(ch => ch.name === "général").send("Hello World ! I'm UP !")
+})
+
+for(directories=["commands","events"],i=0;i<directories.length;i++){
+    if(!fs.existsSync(`${handlerPath}/${directories[i]}`)) fs.mkdirSync(handlerPath + "/" + directories[i])
+}
+
+const cmdFiles = fs.readdirSync(handlerPath + "/commands").filter(file => file.endsWith(".js"))
+for(const file in cmdFiles){
+    const cmd = require(handlerPath + "/commands" + file)
+    client.commands.set(cmd.name, cmd)
+}
+
+bot.on("message", msg => {
+    if(msg.author.bot || !msg.content.startsWith(prefix)) return;
+    const args = msg.content.slice(prefix.lenth).trim().split(/ +/)
+    const cmd = args.shift().toLowerCase();
+
+    try {
+        bot.commands.get(cmd).execute(msg, args)
+    } catch(e){
+        return console.log(`Commande '${cmd}' inconnue`);
+    }
 })
 
 bot.login(process.env.TOKEN)
