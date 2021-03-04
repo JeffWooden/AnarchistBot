0e179d160ad6ff0716765094dd338b5aff012500
Jeff Wooden <jeffwooden.courriel@gmail.com>
2021-03-04 21:42:38 +0100

    Paste default code

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+const config = require("./config.json")
+
+client.once("ready", () => {
+    console.log("Ready !")
+})
+
+client.login(process.env.TOKEN)
