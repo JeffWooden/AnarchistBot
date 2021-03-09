5b2c5dc31669a8442f2db72a899f05e8b9af58d9
Jeff Wooden <jeffwooden.courriel@gmail.com>
2021-03-09 23:06:17 +0100

    Added vote command to command handler

---
+++ handler/commands/vote.js
@@ -0,0 +1,14 @@
+module.exports = {
+    /*
+              __  ___  ___
+        \  / /  \  |  |__ 
+         \/  \__/  |  |___
+    
+    */
+    name: "vote",
+    description: "Lance un vote à l'issu duquel la communauté tranchera",
+    syntax: "vote (mute|ban|config) <parameter> [second parameter] <reason>",
+    execute(msg, args, config){
+
+    }
+}
